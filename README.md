47650f90b243d29fecde3adc5cde6f5c21bff668
aiocat <aiocat@protonmail.com>
2022-06-09 01:04:01 +0300

    enable rustls for reqwest

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+<!--
+ Copyright (c) 2022 aiocat
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+
 # RipGVC
 Profile visit counter for GitHub.
 
--- src/utils.rs
+++ src/utils.rs
@@ -5,8 +5,12 @@
 
 // check github username to see if it exists
 pub async fn check_github_username(username: &str) -> bool {
-    let client = reqwest::Client::new();
+    let client = reqwest::Client::builder();
+
     let response = client
+        .use_rustls_tls()
+        .build()
+        .expect("Failed to build HTTP Client")
         .get(format!("https://github.com/{}", username))
         .send()
         .await
